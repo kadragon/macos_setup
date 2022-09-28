be96f1b4057869a02790661737ac09583af23415
kangdonguk <kangdongouk@gmail.com>
2022-09-28 17:16:18 +0900

    2022.09.28

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 # brew install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/kadragon/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # brew checking
 brew doctor
 
